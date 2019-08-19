0f58580ce93307010d97445dced8abe848af5e19
Atomilin <50828840+Atomilin@users.noreply.github.com>
2019-08-19 09:56:49 +0300

    homework_5-1

--- src/UseStateHook/UseStateHook.js
+++ src/UseStateHook/UseStateHook.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, {useReducer} from "react";
 
 /*
     Напишите компонент с двуми инпутами и кнопкой
@@ -22,4 +22,71 @@ import React, { useState } from "react";
     https://reactjs.org/docs/hooks-reference.html#usereducer
 */
 
-export const Form = () => {};
+function reducer(state, action) {
+    switch (action.type) {
+      case "set_email":
+        return {
+          ...state,
+          email: action.value
+        };
+      case "set_password":
+        return {
+          ...state,
+          password: action.value
+        };
+      case "submit":
+        return {
+          ...state,
+          success: state.email && state.password
+        };
+      default:
+    }
+  }
+
+export const Form = () => {
+    const [state, dispatch] = useReducer(reducer, {
+        email: "",
+        password: "",
+        success: false
+      });
+    
+      function handleInput(evt) {
+        const {
+          target: { name, value }
+        } = evt;
+    
+        dispatch({
+          type: `set_${name}`,
+          value
+        });
+      }
+    
+      function handleClick() {
+        dispatch({
+          type: "submit"
+        });
+      }
+    
+      return (
+        <div>
+          <input
+            data-testid="email-input"
+            type="text"
+            name="email"
+            value={state.email}
+            onChange={handleInput}
+          />
+          <input
+            data-testid="password-input"
+            type="password"
+            name="password"
+            value={state.password}
+            onChange={handleInput}
+          />
+          <button data-testid="submit" onClick={handleClick}>
+            Submit
+          </button>
+          {state.success && <div data-testid="success-message">Вы вошли</div>}
+        </div>
+      );
+    };
